72ed70d549e7ca9290e482144a51cdc8a97bc088
taeyoon <taeyoon@taeyoonui-MacBookPro.local>
2025-06-26 19:19:03 +0900

    review vo

--- matzip/src/main/java/com/kh/matzip/review/model/vo/Review.java
+++ matzip/src/main/java/com/kh/matzip/review/model/vo/Review.java
@@ -1,5 +1,20 @@
 package com.kh.matzip.review.model.vo;
 
+import java.sql.Date;
+
+import lombok.Builder;
+import lombok.Value;
+
+
+@Value
+@Builder
 public class Review {
+
+    private Long reviewNo;
+    private Long reservationNo;
+    private Long userNo;
+    public String reviewContent;
+    public double storeGrade;
+    public Date createDate;
     
 }
