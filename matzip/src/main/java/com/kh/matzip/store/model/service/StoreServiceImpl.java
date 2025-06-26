fa92480acfbecbbddc3f4f8954fe06d28dea1402
taeyoon <taeyoon@taeyoonui-MacBookPro.local>
2025-06-26 19:39:10 +0900

    add store,review mapper xml

--- matzip/src/main/java/com/kh/matzip/store/model/service/StoreServiceImpl.java
+++ matzip/src/main/java/com/kh/matzip/store/model/service/StoreServiceImpl.java
@@ -1,5 +1,13 @@
 package com.kh.matzip.store.model.service;
 
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class StoreServiceImpl implements StoreService {
     
 }
