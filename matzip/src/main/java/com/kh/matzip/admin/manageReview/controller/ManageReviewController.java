581a990787fa900df064013d235c3aab377a9975
[2Seongmin] <tjdrmffktm0928@gmail.com>
2025-07-07 15:09:32 +0900

    fix/manageList

--- matzip/src/main/java/com/kh/matzip/admin/manageReview/controller/ManageReviewController.java
+++ matzip/src/main/java/com/kh/matzip/admin/manageReview/controller/ManageReviewController.java
@@ -1,4 +1,4 @@
-package com.kh.matzip.admin.manageStore.controller;
+package com.kh.matzip.admin.manageReview.controller;
 
 import java.util.Map;
 
@@ -9,17 +9,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.kh.matzip.admin.manageStore.model.service.ManageStoreService;
+import com.kh.matzip.admin.manageReview.model.service.ManageReviewService;
 import com.kh.matzip.member.model.vo.CustomUserDetails;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/admin/manage/stores")
+@RequestMapping("/api/admin/manage/reviews")
 @RequiredArgsConstructor
-public class ManageStoreController {
+public class ManageReviewController {
 
-    private final ManageStoreService manageStoreService;
+    private final ManageReviewService manageReviewService;
 
     @GetMapping
     public ResponseEntity<?> getStoreListByAdmin(
@@ -27,7 +27,7 @@ public class ManageStoreController {
     		@RequestParam(name = "page", defaultValue = "0") int page,
 			@RequestParam(name = "size", defaultValue = "5") int size) {
     	
-        Map<String, Object> result = manageStoreService.getStoreListByAdmin(page, size);
+        Map<String, Object> result = manageReviewService.getReviewListByAdmin(page, size);
         return ResponseEntity.ok(result);
     }
 }
